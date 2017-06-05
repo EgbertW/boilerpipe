65b7d788c68ba30c26d14876fc8771b50896bffc
dankl <dankl@comfylight.com>
2017-06-05 18:21:34 +0200

    Extracted HTMLHighlighterContentHandler from HTMLHighlighter

--- boilerpipe-common/src/main/java/com/kohlschutter/boilerpipe/sax/HTMLHighlighter.java
+++ boilerpipe-common/src/main/java/com/kohlschutter/boilerpipe/sax/HTMLHighlighter.java
@@ -64,6 +64,10 @@ public final class HTMLHighlighter {
     return new HTMLHighlighter(true);
   }
 
+
+  private HTMLHighlighterContentHandler contentHandler = new HTMLHighlighterContentHandler();
+
+
   private HTMLHighlighter(final boolean extractHTML) {
     if (extractHTML) {
       setOutputHighlightOnly(true);
@@ -102,10 +106,10 @@ public final class HTMLHighlighter {
   public String process(final TextDocument doc, final InputSource is)
       throws BoilerpipeProcessingException {
     final Implementation implementation = new Implementation();
-    implementation.process(doc, is);
+    implementation.process(doc, is, contentHandler);
 
-    String html = implementation.html.toString();
-    if (outputHighlightOnly) {
+    String html = contentHandler.html.toString();
+    if (contentHandler.isOutputHighlightOnly()) {
       Matcher m;
 
       boolean repeat = true;
@@ -152,20 +156,12 @@ public final class HTMLHighlighter {
     return process(doc, is);
   }
 
-  private boolean outputHighlightOnly = false;
-  private String extraStyleSheet = "\n<style type=\"text/css\">\n" + ".x-boilerpipe-mark1 {"
-      + " text-decoration:none; " + "background-color: #ffff42 !important; "
-      + "color: black !important; " + "display:inline !important; "
-      + "visibility:visible !important; }\n" + //
-      "</style>\n";
-  private String preHighlight = "<span class=\"x-boilerpipe-mark1\">";
-  private String postHighlight = "</span>";
 
   /**
    * If true, only HTML enclosed within highlighted content will be returned
    */
   public boolean isOutputHighlightOnly() {
-    return outputHighlightOnly;
+    return contentHandler.isOutputHighlightOnly();
   }
 
   /**
@@ -173,7 +169,7 @@ public final class HTMLHighlighter {
    * document.
    */
   public void setOutputHighlightOnly(boolean outputHighlightOnly) {
-    this.outputHighlightOnly = outputHighlightOnly;
+    this.contentHandler.setOutputHighlightOnly(outputHighlightOnly);
   }
 
   /**
@@ -183,7 +179,7 @@ public final class HTMLHighlighter {
    * "x-boilerpipe-mark1" as inline text with yellow background.
    */
   public String getExtraStyleSheet() {
-    return extraStyleSheet;
+    return contentHandler.getExtraStyleSheet();
   }
 
   /**
@@ -194,7 +190,7 @@ public final class HTMLHighlighter {
    * @param extraStyleSheet Plain HTML
    */
   public void setExtraStyleSheet(String extraStyleSheet) {
-    this.extraStyleSheet = extraStyleSheet;
+    this.contentHandler.setExtraStyleSheet(extraStyleSheet);
   }
 
   /**
@@ -203,7 +199,7 @@ public final class HTMLHighlighter {
    * By default, this corresponds to <code>&lt;span class=&qupt;x-boilerpipe-mark1&quot;&gt;</code>
    */
   public String getPreHighlight() {
-    return preHighlight;
+    return contentHandler.getPreHighlight();
   }
 
   /**
@@ -212,7 +208,7 @@ public final class HTMLHighlighter {
    * To disable, set it to the empty string: ""
    */
   public void setPreHighlight(String preHighlight) {
-    this.preHighlight = preHighlight;
+    this.contentHandler.setPreHighlight(preHighlight);
   }
 
   /**
@@ -221,7 +217,7 @@ public final class HTMLHighlighter {
    * By default, this corresponds to <code>&lt;/span&gt;</code>
    */
   public String getPostHighlight() {
-    return postHighlight;
+    return contentHandler.getPostHighlight();
   }
 
   /**
@@ -230,86 +226,30 @@ public final class HTMLHighlighter {
    * To disable, set it to the empty string: ""
    */
   public void setPostHighlight(String postHighlight) {
-    this.postHighlight = postHighlight;
+    this.contentHandler.setPostHighlight(postHighlight);
   }
 
-  private abstract static class TagAction {
-    void beforeStart(final Implementation instance, final String localName) {
-    }
 
-    void afterStart(final Implementation instance, final String localName) {
-    }
-
-    void beforeEnd(final Implementation instance, final String localName) {
-    }
-
-    void afterEnd(final Implementation instance, final String localName) {
-    }
-  }
-
-  private static final TagAction TA_IGNORABLE_ELEMENT = new TagAction() {
-    void beforeStart(final Implementation instance, final String localName) {
-      instance.inIgnorableElement++;
-    }
-
-    void afterEnd(final Implementation instance, final String localName) {
-      instance.inIgnorableElement--;
-    }
-  };
-
-  private static final TagAction TA_HEAD = new TagAction() {
-    void beforeStart(final Implementation instance, final String localName) {
-      instance.inIgnorableElement++;
-    }
-
-    void beforeEnd(final Implementation instance, String localName) {
-      instance.html.append(instance.hl.extraStyleSheet);
-    }
-
-    void afterEnd(final Implementation instance, final String localName) {
-      instance.inIgnorableElement--;
-    }
-  };
-  private static Map<String, TagAction> TAG_ACTIONS = new HashMap<String, TagAction>();
-  static {
-    TAG_ACTIONS.put("STYLE", TA_IGNORABLE_ELEMENT);
-    TAG_ACTIONS.put("SCRIPT", TA_IGNORABLE_ELEMENT);
-    TAG_ACTIONS.put("OPTION", TA_IGNORABLE_ELEMENT);
-    TAG_ACTIONS.put("NOSCRIPT", TA_IGNORABLE_ELEMENT);
-    TAG_ACTIONS.put("OBJECT", TA_IGNORABLE_ELEMENT);
-    TAG_ACTIONS.put("EMBED", TA_IGNORABLE_ELEMENT);
-    TAG_ACTIONS.put("APPLET", TA_IGNORABLE_ELEMENT);
-    // NOTE: you might want to comment this out:
-    TAG_ACTIONS.put("LINK", TA_IGNORABLE_ELEMENT);
-
-    TAG_ACTIONS.put("HEAD", TA_HEAD);
-  }
-
-  private final class Implementation extends AbstractSAXParser implements ContentHandler {
-    StringBuilder html = new StringBuilder();
-
-    private int inIgnorableElement = 0;
-    private int characterElementIdx = 0;
-    private final BitSet contentBitSet = new BitSet();
-    private final HTMLHighlighter hl = HTMLHighlighter.this;
+  private final class Implementation extends AbstractSAXParser {
 
     Implementation() {
       super(new HTMLConfiguration());
-      setContentHandler(this);
     }
 
-    void process(final TextDocument doc, final InputSource is) throws BoilerpipeProcessingException {
+    void process(final TextDocument doc, final InputSource inputSource, HTMLHighlighterContentHandler contentHandler) throws BoilerpipeProcessingException {
       for (TextBlock block : doc.getTextBlocks()) {
         if (block.isContent()) {
           final BitSet bs = block.getContainedTextElements();
           if (bs != null) {
-            contentBitSet.or(bs);
+            contentHandler.getContentBitSet().or(bs);
           }
         }
       }
 
       try {
-        parse(is);
+        setContentHandler(contentHandler);
+
+        parse(inputSource);
       } catch (SAXException e) {
         throw new BoilerpipeProcessingException(e);
       } catch (IOException e) {
@@ -317,187 +257,6 @@ public final class HTMLHighlighter {
       }
     }
 
-    public void endDocument() throws SAXException {
-    }
-
-    public void endPrefixMapping(String prefix) throws SAXException {
-    }
-
-    public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
-    }
-
-    public void processingInstruction(String target, String data) throws SAXException {
-    }
-
-    public void setDocumentLocator(Locator locator) {
-    }
-
-    public void skippedEntity(String name) throws SAXException {
-    }
-
-    public void startDocument() throws SAXException {
-    }
-
-    public void startElement(String uri, String localName, String qName, Attributes atts)
-        throws SAXException {
-      TagAction ta = TAG_ACTIONS.get(localName);
-      if (ta != null) {
-        ta.beforeStart(this, localName);
-      }
-
-      // HACK: remove existing highlight
-      boolean ignoreAttrs = false;
-      if ("SPAN".equalsIgnoreCase(localName)) {
-        String classVal = atts.getValue("class");
-        if ("x-boilerpipe-mark1".equals(classVal)) {
-          ignoreAttrs = true;
-        }
-      }
-
-      try {
-        if (inIgnorableElement == 0) {
-          if (outputHighlightOnly) {
-            // boolean highlight = contentBitSet
-            // .get(characterElementIdx);
-
-            // if (!highlight) {
-            // return;
-            // }
-          }
-
-          final Set<String> whitelistAttributes;
-          if (tagWhitelist == null) {
-            whitelistAttributes = null;
-          } else {
-            whitelistAttributes = tagWhitelist.get(qName);
-            if (whitelistAttributes == null) {
-              // skip
-              return;
-            }
-          }
-
-          html.append('<');
-          html.append(qName);
-          if (!ignoreAttrs) {
-            final int numAtts = atts.getLength();
-            for (int i = 0; i < numAtts; i++) {
-              final String attr = atts.getQName(i);
-
-              if (whitelistAttributes != null && !whitelistAttributes.contains(attr)) {
-                // skip
-                continue;
-              }
-
-              final String value = atts.getValue(i);
-              html.append(' ');
-              html.append(attr);
-              html.append("=\"");
-              html.append(xmlEncode(value));
-              html.append("\"");
-            }
-          }
-          html.append('>');
-        }
-      } finally {
-        if (ta != null) {
-          ta.afterStart(this, localName);
-        }
-      }
-    }
-
-    public void endElement(String uri, String localName, String qName) throws SAXException {
-      TagAction ta = TAG_ACTIONS.get(localName);
-      if (ta != null) {
-        ta.beforeEnd(this, localName);
-      }
-
-      try {
-        if (inIgnorableElement == 0) {
-          if (outputHighlightOnly) {
-            // boolean highlight = contentBitSet
-            // .get(characterElementIdx);
-
-            // if (!highlight) {
-            // return;
-            // }
-          }
-
-          if (tagWhitelist != null && !tagWhitelist.containsKey(qName)) {
-            // skip
-            return;
-          }
-
-          html.append("</");
-          html.append(qName);
-          html.append('>');
-        }
-      } finally {
-        if (ta != null) {
-          ta.afterEnd(this, localName);
-        }
-      }
-    }
-
-    public void characters(char[] ch, int start, int length) throws SAXException {
-      characterElementIdx++;
-      if (inIgnorableElement == 0) {
-
-        boolean highlight = contentBitSet.get(characterElementIdx);
-
-        if (!highlight && outputHighlightOnly) {
-          return;
-        }
-
-        if (highlight) {
-          html.append(preHighlight);
-        }
-        html.append(xmlEncode(String.valueOf(ch, start, length)));
-        if (highlight) {
-          html.append(postHighlight);
-        }
-      }
-    }
-
-    public void startPrefixMapping(String prefix, String uri) throws SAXException {
-    }
-
   }
 
-  private static String xmlEncode(final String in) {
-    if (in == null) {
-      return "";
-    }
-    char c;
-    StringBuilder out = new StringBuilder(in.length());
-
-    for (int i = 0; i < in.length(); i++) {
-      c = in.charAt(i);
-      switch (c) {
-        case '<':
-          out.append("&lt;");
-          break;
-        case '>':
-          out.append("&gt;");
-          break;
-        case '&':
-          out.append("&amp;");
-          break;
-        case '"':
-          out.append("&quot;");
-          break;
-        default:
-          out.append(c);
-      }
-    }
-
-    return out.toString();
-  }
-
-  public Map<String, Set<String>> getTagWhitelist() {
-    return tagWhitelist;
-  }
-
-  public void setTagWhitelist(Map<String, Set<String>> tagWhitelist) {
-    this.tagWhitelist = tagWhitelist;
-  }
 }
---
+++ boilerpipe-common/src/main/java/com/kohlschutter/boilerpipe/sax/HTMLHighlighterContentHandler.java
@@ -0,0 +1,369 @@
+package com.kohlschutter.boilerpipe.sax;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+
+import java.util.BitSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+
+public class HTMLHighlighterContentHandler implements ContentHandler {
+
+    StringBuilder html = new StringBuilder();
+
+    private boolean outputHighlightOnly = false;
+
+    private String extraStyleSheet = "\n<style type=\"text/css\">\n" + ".x-boilerpipe-mark1 {"
+        + " text-decoration:none; " + "background-color: #ffff42 !important; "
+        + "color: black !important; " + "display:inline !important; "
+        + "visibility:visible !important; }\n" + //
+        "</style>\n";
+
+    private String preHighlight = "<span class=\"x-boilerpipe-mark1\">";
+    private String postHighlight = "</span>";
+
+    private int inIgnorableElement = 0;
+    private int characterElementIdx = 0;
+    private BitSet contentBitSet = new BitSet();
+
+    private Map<String, Set<String>> tagWhitelist = null;
+
+
+    public StringBuilder getHtml() {
+        return html;
+    }
+
+    public void setHtml(StringBuilder html) {
+        this.html = html;
+    }
+
+    public BitSet getContentBitSet() {
+        return contentBitSet;
+    }
+
+    public void setContentBitSet(BitSet contentBitSet) {
+        this.contentBitSet = contentBitSet;
+    }
+
+    /**
+     * If true, only HTML enclosed within highlighted content will be returned
+     */
+    public boolean isOutputHighlightOnly() {
+        return outputHighlightOnly;
+    }
+
+    /**
+     * Sets whether only HTML enclosed within highlighted content will be returned, or the whole HTML
+     * document.
+     */
+    public void setOutputHighlightOnly(boolean outputHighlightOnly) {
+        this.outputHighlightOnly = outputHighlightOnly;
+    }
+
+    /**
+     * Returns the extra stylesheet definition that will be inserted in the HEAD element.
+     *
+     * By default, this corresponds to a simple definition that marks text in class
+     * "x-boilerpipe-mark1" as inline text with yellow background.
+     */
+    public String getExtraStyleSheet() {
+        return extraStyleSheet;
+    }
+
+    /**
+     * Sets the extra stylesheet definition that will be inserted in the HEAD element.
+     *
+     * To disable, set it to the empty string: ""
+     *
+     * @param extraStyleSheet Plain HTML
+     */
+    public void setExtraStyleSheet(String extraStyleSheet) {
+        this.extraStyleSheet = extraStyleSheet;
+    }
+
+    /**
+     * Returns the string that will be inserted before any highlighted HTML block.
+     *
+     * By default, this corresponds to <code>&lt;span class=&qupt;x-boilerpipe-mark1&quot;&gt;</code>
+     */
+    public String getPreHighlight() {
+        return preHighlight;
+    }
+
+    /**
+     * Sets the string that will be inserted prior to any highlighted HTML block.
+     *
+     * To disable, set it to the empty string: ""
+     */
+    public void setPreHighlight(String preHighlight) {
+        this.preHighlight = preHighlight;
+    }
+
+    /**
+     * Returns the string that will be inserted after any highlighted HTML block.
+     *
+     * By default, this corresponds to <code>&lt;/span&gt;</code>
+     */
+    public String getPostHighlight() {
+        return postHighlight;
+    }
+
+    /**
+     * Sets the string that will be inserted after any highlighted HTML block.
+     *
+     * To disable, set it to the empty string: ""
+     */
+    public void setPostHighlight(String postHighlight) {
+        this.postHighlight = postHighlight;
+    }
+
+    public void incrementInIgnorableElement() {
+        inIgnorableElement++;
+    }
+
+    public void decrementInIgnorableElement() {
+        inIgnorableElement--;
+    }
+
+
+    public void endDocument() throws SAXException {
+    }
+
+    public void endPrefixMapping(String prefix) throws SAXException {
+    }
+
+    public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
+    }
+
+    public void processingInstruction(String target, String data) throws SAXException {
+    }
+
+    public void setDocumentLocator(Locator locator) {
+    }
+
+    public void skippedEntity(String name) throws SAXException {
+    }
+
+    public void startDocument() throws SAXException {
+    }
+
+    public void startElement(String uri, String localName, String qName, Attributes atts)
+        throws SAXException {
+        TagAction ta = TAG_ACTIONS.get(localName);
+        if (ta != null) {
+            ta.beforeStart(this, localName);
+        }
+
+        // HACK: remove existing highlight
+        boolean ignoreAttrs = false;
+        if ("SPAN".equalsIgnoreCase(localName)) {
+            String classVal = atts.getValue("class");
+            if ("x-boilerpipe-mark1".equals(classVal)) {
+                ignoreAttrs = true;
+            }
+        }
+
+        try {
+            if (inIgnorableElement == 0) {
+                if (outputHighlightOnly) {
+                    // boolean highlight = contentBitSet
+                    // .get(characterElementIdx);
+
+                    // if (!highlight) {
+                    // return;
+                    // }
+                }
+
+                final Set<String> whitelistAttributes;
+                if (tagWhitelist == null) {
+                    whitelistAttributes = null;
+                } else {
+                    whitelistAttributes = tagWhitelist.get(qName);
+                    if (whitelistAttributes == null) {
+                        // skip
+                        return;
+                    }
+                }
+
+                html.append('<');
+                html.append(qName);
+                if (!ignoreAttrs) {
+                    final int numAtts = atts.getLength();
+                    for (int i = 0; i < numAtts; i++) {
+                        final String attr = atts.getQName(i);
+
+                        if (whitelistAttributes != null && !whitelistAttributes.contains(attr)) {
+                            // skip
+                            continue;
+                        }
+
+                        final String value = atts.getValue(i);
+                        html.append(' ');
+                        html.append(attr);
+                        html.append("=\"");
+                        html.append(xmlEncode(value));
+                        html.append("\"");
+                    }
+                }
+                html.append('>');
+            }
+        } finally {
+            if (ta != null) {
+                ta.afterStart(this, localName);
+            }
+        }
+    }
+
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        TagAction ta = TAG_ACTIONS.get(localName);
+        if (ta != null) {
+            ta.beforeEnd(this, localName);
+        }
+
+        try {
+            if (inIgnorableElement == 0) {
+                if (outputHighlightOnly) {
+                    // boolean highlight = contentBitSet
+                    // .get(characterElementIdx);
+
+                    // if (!highlight) {
+                    // return;
+                    // }
+                }
+
+                if (tagWhitelist != null && !tagWhitelist.containsKey(qName)) {
+                    // skip
+                    return;
+                }
+
+                html.append("</");
+                html.append(qName);
+                html.append('>');
+            }
+        } finally {
+            if (ta != null) {
+                ta.afterEnd(this, localName);
+            }
+        }
+    }
+
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        characterElementIdx++;
+        if (inIgnorableElement == 0) {
+
+            boolean highlight = contentBitSet.get(characterElementIdx);
+
+            if (!highlight && outputHighlightOnly) {
+                return;
+            }
+
+            if (highlight) {
+                html.append(preHighlight);
+            }
+            html.append(xmlEncode(String.valueOf(ch, start, length)));
+            if (highlight) {
+                html.append(postHighlight);
+            }
+        }
+    }
+
+    public void startPrefixMapping(String prefix, String uri) throws SAXException {
+    }
+
+
+    private static String xmlEncode(final String in) {
+        if (in == null) {
+            return "";
+        }
+        char c;
+        StringBuilder out = new StringBuilder(in.length());
+
+        for (int i = 0; i < in.length(); i++) {
+            c = in.charAt(i);
+            switch (c) {
+                case '<':
+                    out.append("&lt;");
+                    break;
+                case '>':
+                    out.append("&gt;");
+                    break;
+                case '&':
+                    out.append("&amp;");
+                    break;
+                case '"':
+                    out.append("&quot;");
+                    break;
+                default:
+                    out.append(c);
+            }
+        }
+
+        return out.toString();
+    }
+
+    public Map<String, Set<String>> getTagWhitelist() {
+        return tagWhitelist;
+    }
+
+    public void setTagWhitelist(Map<String, Set<String>> tagWhitelist) {
+        this.tagWhitelist = tagWhitelist;
+    }
+
+    public abstract static class TagAction {
+        void beforeStart(final HTMLHighlighterContentHandler contentHandler, final String localName) {
+        }
+
+        void afterStart(final HTMLHighlighterContentHandler contentHandler, final String localName) {
+        }
+
+        void beforeEnd(final HTMLHighlighterContentHandler contentHandler, final String localName) {
+        }
+
+        void afterEnd(final HTMLHighlighterContentHandler contentHandler, final String localName) {
+        }
+    }
+
+    private static final TagAction TA_IGNORABLE_ELEMENT = new TagAction() {
+        void beforeStart(final HTMLHighlighterContentHandler contentHandler, final String localName) {
+            contentHandler.incrementInIgnorableElement();
+        }
+
+        void afterEnd(final HTMLHighlighterContentHandler contentHandler, final String localName) {
+            contentHandler.decrementInIgnorableElement();
+        }
+    };
+
+    private static final TagAction TA_HEAD = new TagAction() {
+        void beforeStart(final HTMLHighlighterContentHandler contentHandler, final String localName) {
+            contentHandler.incrementInIgnorableElement();
+        }
+
+        void beforeEnd(final HTMLHighlighterContentHandler contentHandler, String localName) {
+            contentHandler.getHtml().append(contentHandler.getExtraStyleSheet());
+        }
+
+        void afterEnd(final HTMLHighlighterContentHandler contentHandler, final String localName) {
+            contentHandler.decrementInIgnorableElement();
+        }
+    };
+
+    public static Map<String, TagAction> TAG_ACTIONS = new HashMap<String, TagAction>();
+    static {
+        TAG_ACTIONS.put("STYLE", TA_IGNORABLE_ELEMENT);
+        TAG_ACTIONS.put("SCRIPT", TA_IGNORABLE_ELEMENT);
+        TAG_ACTIONS.put("OPTION", TA_IGNORABLE_ELEMENT);
+        TAG_ACTIONS.put("NOSCRIPT", TA_IGNORABLE_ELEMENT);
+        TAG_ACTIONS.put("OBJECT", TA_IGNORABLE_ELEMENT);
+        TAG_ACTIONS.put("EMBED", TA_IGNORABLE_ELEMENT);
+        TAG_ACTIONS.put("APPLET", TA_IGNORABLE_ELEMENT);
+        // NOTE: you might want to comment this out:
+        TAG_ACTIONS.put("LINK", TA_IGNORABLE_ELEMENT);
+
+        TAG_ACTIONS.put("HEAD", TA_HEAD);
+    }
+
+}
